9de2b2fdfd9c3ef8038839ca6fdd900c4260ae77
Dima Bobrov <bda29041998@gmail.com>
2018-03-09 15:14:01 +0400

    Базовая версия задания про студентов

---
+++ 2_course/Студенты/Студенты/Form1.Designer.cs
@@ -0,0 +1,316 @@
+﻿namespace Студенты
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.prevBut = new System.Windows.Forms.Button();
+            this.nextBut = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.surnameText = new System.Windows.Forms.TextBox();
+            this.nameText = new System.Windows.Forms.TextBox();
+            this.facText = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.searchText = new System.Windows.Forms.TextBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.файлToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.открытьСписокToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.сохранитьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.студентToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.предыдущийToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.следующийToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.студентToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.добавитьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.удалитьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // prevBut
+            // 
+            this.prevBut.Enabled = false;
+            this.prevBut.Location = new System.Drawing.Point(167, 326);
+            this.prevBut.Name = "prevBut";
+            this.prevBut.Size = new System.Drawing.Size(209, 72);
+            this.prevBut.TabIndex = 0;
+            this.prevBut.Text = "Предыдущий";
+            this.prevBut.UseVisualStyleBackColor = true;
+            this.prevBut.Click += new System.EventHandler(this.prevBut_Click);
+            // 
+            // nextBut
+            // 
+            this.nextBut.Enabled = false;
+            this.nextBut.Location = new System.Drawing.Point(544, 326);
+            this.nextBut.Name = "nextBut";
+            this.nextBut.Size = new System.Drawing.Size(209, 72);
+            this.nextBut.TabIndex = 1;
+            this.nextBut.Text = "Следующий";
+            this.nextBut.UseVisualStyleBackColor = true;
+            this.nextBut.Click += new System.EventHandler(this.nextBut_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(197, 107);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Фамилия";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(197, 181);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Имя";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(197, 259);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(80, 17);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Факультет";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(88, 455);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(48, 17);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Поиск";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Enabled = false;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(184, 455);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(192, 24);
+            this.comboBox1.TabIndex = 6;
+            // 
+            // surnameText
+            // 
+            this.surnameText.Enabled = false;
+            this.surnameText.Location = new System.Drawing.Point(335, 107);
+            this.surnameText.Name = "surnameText";
+            this.surnameText.Size = new System.Drawing.Size(252, 22);
+            this.surnameText.TabIndex = 7;
+            // 
+            // nameText
+            // 
+            this.nameText.Enabled = false;
+            this.nameText.Location = new System.Drawing.Point(335, 181);
+            this.nameText.Name = "nameText";
+            this.nameText.Size = new System.Drawing.Size(252, 22);
+            this.nameText.TabIndex = 8;
+            // 
+            // facText
+            // 
+            this.facText.Enabled = false;
+            this.facText.Location = new System.Drawing.Point(335, 254);
+            this.facText.Name = "facText";
+            this.facText.Size = new System.Drawing.Size(252, 22);
+            this.facText.TabIndex = 9;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(436, 459);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(16, 17);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "=";
+            // 
+            // searchText
+            // 
+            this.searchText.Enabled = false;
+            this.searchText.Location = new System.Drawing.Point(501, 456);
+            this.searchText.Name = "searchText";
+            this.searchText.Size = new System.Drawing.Size(252, 22);
+            this.searchText.TabIndex = 11;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.файлToolStripMenuItem,
+            this.студентToolStripMenuItem,
+            this.студентToolStripMenuItem1});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(914, 28);
+            this.menuStrip1.TabIndex = 12;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // файлToolStripMenuItem
+            // 
+            this.файлToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.открытьСписокToolStripMenuItem,
+            this.сохранитьToolStripMenuItem});
+            this.файлToolStripMenuItem.Name = "файлToolStripMenuItem";
+            this.файлToolStripMenuItem.Size = new System.Drawing.Size(57, 24);
+            this.файлToolStripMenuItem.Text = "Файл";
+            // 
+            // открытьСписокToolStripMenuItem
+            // 
+            this.открытьСписокToolStripMenuItem.Name = "открытьСписокToolStripMenuItem";
+            this.открытьСписокToolStripMenuItem.Size = new System.Drawing.Size(181, 26);
+            this.открытьСписокToolStripMenuItem.Text = "Открыть";
+            this.открытьСписокToolStripMenuItem.Click += new System.EventHandler(this.открытьСписокToolStripMenuItem_Click);
+            // 
+            // сохранитьToolStripMenuItem
+            // 
+            this.сохранитьToolStripMenuItem.Name = "сохранитьToolStripMenuItem";
+            this.сохранитьToolStripMenuItem.Size = new System.Drawing.Size(181, 26);
+            this.сохранитьToolStripMenuItem.Text = "Сохранить";
+            this.сохранитьToolStripMenuItem.Click += new System.EventHandler(this.сохранитьToolStripMenuItem_Click);
+            // 
+            // студентToolStripMenuItem
+            // 
+            this.студентToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.предыдущийToolStripMenuItem,
+            this.следующийToolStripMenuItem});
+            this.студентToolStripMenuItem.Name = "студентToolStripMenuItem";
+            this.студентToolStripMenuItem.Size = new System.Drawing.Size(92, 24);
+            this.студентToolStripMenuItem.Text = "Просмотр";
+            // 
+            // предыдущийToolStripMenuItem
+            // 
+            this.предыдущийToolStripMenuItem.Enabled = false;
+            this.предыдущийToolStripMenuItem.Name = "предыдущийToolStripMenuItem";
+            this.предыдущийToolStripMenuItem.Size = new System.Drawing.Size(181, 26);
+            this.предыдущийToolStripMenuItem.Text = "Предыдущий";
+            this.предыдущийToolStripMenuItem.Click += new System.EventHandler(this.предыдущийToolStripMenuItem_Click);
+            // 
+            // следующийToolStripMenuItem
+            // 
+            this.следующийToolStripMenuItem.Enabled = false;
+            this.следующийToolStripMenuItem.Name = "следующийToolStripMenuItem";
+            this.следующийToolStripMenuItem.Size = new System.Drawing.Size(181, 26);
+            this.следующийToolStripMenuItem.Text = "Следующий";
+            this.следующийToolStripMenuItem.Click += new System.EventHandler(this.следующийToolStripMenuItem_Click);
+            // 
+            // студентToolStripMenuItem1
+            // 
+            this.студентToolStripMenuItem1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.добавитьToolStripMenuItem,
+            this.удалитьToolStripMenuItem});
+            this.студентToolStripMenuItem1.Name = "студентToolStripMenuItem1";
+            this.студентToolStripMenuItem1.Size = new System.Drawing.Size(74, 24);
+            this.студентToolStripMenuItem1.Text = "Студент";
+            // 
+            // добавитьToolStripMenuItem
+            // 
+            this.добавитьToolStripMenuItem.Name = "добавитьToolStripMenuItem";
+            this.добавитьToolStripMenuItem.Size = new System.Drawing.Size(181, 26);
+            this.добавитьToolStripMenuItem.Text = "Добавить";
+            this.добавитьToolStripMenuItem.Click += new System.EventHandler(this.добавитьToolStripMenuItem_Click);
+            // 
+            // удалитьToolStripMenuItem
+            // 
+            this.удалитьToolStripMenuItem.Enabled = false;
+            this.удалитьToolStripMenuItem.Name = "удалитьToolStripMenuItem";
+            this.удалитьToolStripMenuItem.Size = new System.Drawing.Size(181, 26);
+            this.удалитьToolStripMenuItem.Text = "Удалить";
+            this.удалитьToolStripMenuItem.Click += new System.EventHandler(this.удалитьToolStripMenuItem_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.Filter = "(*.xml)|*.xml";
+            // 
+            // saveFileDialog1
+            // 
+            this.saveFileDialog1.Filter = "(*.xml)|*.xml";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(914, 524);
+            this.Controls.Add(this.searchText);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.facText);
+            this.Controls.Add(this.nameText);
+            this.Controls.Add(this.surnameText);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.nextBut);
+            this.Controls.Add(this.prevBut);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Студенты";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button prevBut;
+        private System.Windows.Forms.Button nextBut;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox surnameText;
+        private System.Windows.Forms.TextBox nameText;
+        private System.Windows.Forms.TextBox facText;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox searchText;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem файлToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem открытьСписокToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem сохранитьToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem студентToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem предыдущийToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem следующийToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem студентToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem добавитьToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem удалитьToolStripMenuItem;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+    }
+}
+
---
+++ 2_course/Студенты/Студенты/Form1.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Serialization;
+
+namespace Студенты
+{
+
+    public partial class Form1 : Form
+    {
+        public List<Student> students = new List<Student>();
+        public int curSt = -1;
+        public Form1()
+        {
+            InitializeComponent();
+            //Blocked();
+        }
+
+        private void открытьСписокToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.Cancel)
+                return;
+
+            using (FileStream fs = new FileStream(openFileDialog1.FileName, FileMode.OpenOrCreate))
+            {
+
+                XmlSerializer formatter = new XmlSerializer(typeof(List<Student>));
+                students = (List<Student>)formatter.Deserialize(fs);
+                if (students.Count != 0)
+                {
+                    curSt = 0;
+                }
+                Blocked();
+                if (curSt != -1)
+                {
+                    ShowSt();
+                }
+            }
+        }
+        private void сохранитьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (saveFileDialog1.ShowDialog() == DialogResult.Cancel)
+                return;
+
+            XmlSerializer formatter = new XmlSerializer(typeof(List<Student>));
+
+            using (FileStream fs = new FileStream(saveFileDialog1.FileName, FileMode.OpenOrCreate))
+            {
+                formatter.Serialize(fs, students);
+            }
+        }
+
+        private void добавитьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            students.Add(new Student() { Surname = "", Name = "", Faculty = "" });
+            if (curSt == -1)
+            {
+                ++curSt;
+            }
+            Change();
+            ShowSt();
+            Blocked();
+        }
+        private void удалитьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            students.Remove(students[curSt]);
+            if (curSt == students.Count) --curSt;
+            ShowSt();
+            Blocked();
+        }
+
+        private void Blocked()
+        {
+            //if (students == null)
+                //return;
+            if (curSt == -1)
+            {
+                удалитьToolStripMenuItem.Enabled = false;
+                surnameText.Enabled = false;
+                nameText.Enabled = false;
+                facText.Enabled = false;
+                searchText.Enabled = false;
+                prevBut.Enabled = false;
+                nextBut.Enabled = false;
+                предыдущийToolStripMenuItem.Enabled = false;
+                следующийToolStripMenuItem.Enabled = false;
+                comboBox1.Enabled = false;
+            }
+            else
+            {
+                удалитьToolStripMenuItem.Enabled = true;
+                surnameText.Enabled = true;
+                nameText.Enabled = true;
+                facText.Enabled = true;
+                searchText.Enabled = true;
+                prevBut.Enabled = true;
+                nextBut.Enabled = true;
+                предыдущийToolStripMenuItem.Enabled = true;
+                следующийToolStripMenuItem.Enabled = true;
+                comboBox1.Enabled = true;
+            }
+
+            if (curSt == 0 || curSt == -1)
+            {
+                предыдущийToolStripMenuItem.Enabled = false;
+                prevBut.Enabled = false;
+            }
+            else
+            {
+                предыдущийToolStripMenuItem.Enabled = true;
+                prevBut.Enabled = true;
+            }
+            if (curSt == students.Count - 1)
+            {
+                следующийToolStripMenuItem.Enabled = false;
+                nextBut.Enabled = false;
+            }
+            else
+            {
+                следующийToolStripMenuItem.Enabled = true;
+                nextBut.Enabled = true;
+            }
+        }
+
+        private void ShowSt()
+        {
+            if (curSt != -1)
+            {
+                surnameText.Text = students[curSt].Surname;
+                nameText.Text = students[curSt].Name;
+                facText.Text = students[curSt].Faculty; 
+            }
+            else
+            {
+                surnameText.Text = "";
+                nameText.Text = "";
+                facText.Text = "";
+            }
+        }
+
+        private void Change()
+        {
+            students[curSt].Surname = surnameText.Text;
+            students[curSt].Name = nameText.Text;
+            students[curSt].Faculty = facText.Text;
+        }
+
+        private void предыдущийToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Change();
+            --curSt;
+            ShowSt();
+            Blocked();
+        }
+
+        private void следующийToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Change();
+            ++curSt;
+            ShowSt();
+            Blocked();
+        }
+
+        private void prevBut_Click(object sender, EventArgs e)
+        {
+            Change();
+            --curSt;
+            ShowSt();
+            Blocked();
+        }
+
+        private void nextBut_Click(object sender, EventArgs e)
+        {
+            Change();
+            ++curSt;
+            ShowSt();
+            Blocked();
+        }
+
+        
+    }
+
+    [Serializable]
+    public class Student
+    {
+        public string Surname { get; set; }
+        public string Name { get; set; }
+        public string Faculty { get; set; }
+
+        public Student()
+        {
+        }
+    }
+}
